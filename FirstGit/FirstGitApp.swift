2a900eb94459c5a55533261fc3f8114a2a6e0100
PiaPoco <piapia0618@docomo.ne.jp>
2020-11-07 08:13:32 +0900

    Initial Commit

---
+++ FirstGit/FirstGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstGitApp.swift
+//  FirstGit
+//
+//  Created by PiaPoco on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct FirstGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
